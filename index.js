4425b3430f07e92a0e862bc6678ce988b4b1222e
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-14 11:34:10 -0700

    config to deploy on heroku

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// a hosting service might assign a port dynamically. use an environment variable instead
+// Environment Variables are system wide. local comp vs. remote server have different values. need to tell Node how to get these values.
+
+const port = process.env.PORT || 4000; // use 4000 if not provided in .env
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
